ca0cc69ec0d25991842d3f795c2b230c3e4e2606
Virean <gama.bruno.196@gmail.com>
2025-05-26 22:12:32 +0100

    Updating main.py, adding circleshape.py, asteroid.py, player.py and asteroidfield.py

--- main.py
+++ main.py
@@ -1,17 +1,45 @@
 import pygame
+import sys
 from constants import *
+from player import *
+from circleshape import *
+from asteroid import *
+from asteroidfield import *
+
+updatable = pygame.sprite.Group()
+drawable = pygame.sprite.Group()
+asteroids = pygame.sprite.Group()
+AsteroidField.containers = (updatable)
+Player.containers = (updatable, drawable)
+Asteroid.containers = (asteroids, updatable, drawable)
 
 def main():
+    pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+    asteroid_field = AsteroidField()
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill((255,255,255))
+        screen.fill((0,0,0))
+        updatable.update(dt)
+        for asteroid in asteroids:
+            if asteroid.collision_check(player):
+                print("Game Over!")
+                sys.exit()
+        for sprite in drawable:
+            sprite.draw(screen)
         pygame.display.flip()
+        clock.tick(60)
+        dt = clock.tick(60)/1000
     #print("Starting Asteroids!")
     #print(f"Screen width: {SCREEN_WIDTH}")
     #print(f"Screen height: {SCREEN_HEIGHT}")
+
+
 
 if __name__ == "__main__":
     main()
